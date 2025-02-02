06889c6bc6c7172c698db271cc9450721ffbeb83
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-02-02 11:36:08 +0200

    fix: change settings to connect database

--- core/database.py
+++ core/database.py
@@ -6,10 +6,9 @@ from core.config import settings
 class Base(DeclarativeBase):
     pass
 
+connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
 
-engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
-)
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args, echo=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
